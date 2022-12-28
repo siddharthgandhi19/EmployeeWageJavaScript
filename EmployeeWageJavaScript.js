5ecd5930c52728eeec236ece37897f3bf2bb5149
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-28 08:34:18 +0530

    [Siddharth]Add: Checking employee is present or absent

--- EmployeeWageJavaScript.js
+++ EmployeeWageJavaScript.js
@@ -2,9 +2,9 @@ const IS_PRESENT = 0;
 let empCheck = Math.floor(Math.random() * 10 % 2 );
 if (empCheck == IS_PRESENT)
 {
-    console.log("Emp is Absent");
+    console.log("Employee is Absent");
 }
 else
 {
-    console.log("Emp is Present");
+    console.log("Employee is Present");
 }
