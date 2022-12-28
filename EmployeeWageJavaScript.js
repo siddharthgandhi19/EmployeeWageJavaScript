acd05c41938932846f28c05864a5dd0707922415
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-28 08:41:41 +0530

    [Siddharth]Add: Employee Income

--- EmployeeWageJavaScript.js
+++ EmployeeWageJavaScript.js
@@ -8,3 +8,24 @@ else
 {
     console.log("Employee is Present");
 }
+
+
+//UC-2
+const IS_PART_TIME = 1 , IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4 , FULL_TIME_HRS = 8;
+const WAGE_PER_HRS = 20;
+let empHrs = 0
+empCheck = Math.floor(Math.random() * 10 % 3 );
+switch (empCheck)
+{
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        break;
+    default:
+        empHrs=0;
+}
+let empWage = empHrs * WAGE_PER_HRS;
+console.log("Emp. Wage :- " + empWage);
